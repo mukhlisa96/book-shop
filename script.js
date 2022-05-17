58c4372d9db7bd104c1ef183b9ac248f256e62f0
mukhlisa96 <mukhlisakhonkhusanboyeva@gmail.com>
2022-05-17 17:55:15 +0500

    init: start book-shop

---
+++ script.js
@@ -0,0 +1,16 @@
+script,js
+const body = document.body
+const header = document.createElement("header")
+const main = document.createElement("main")
+const footer = document.createElement("footer")
+body.append(header)
+body.append(main)
+body.append(footer)
+
+
+var card = document.createElement("div")
+card.classList.add("card")
+
+main.append(card)
+
+js dom практика
